691d9fd96d010a39045579e0080363ba5864efc5
mimiPC <haru869pc@gmail.com>
2025-12-15 00:02:00 +0900

    1215

--- service1_http/script.js
+++ service1_http/script.js
@@ -30,6 +30,6 @@ const params = {
 };
 
 export default function () {
-  const res = http.get('http://<IP address>', params);
+  const res = http.get('http://34.146.83.214/', params);
   check(res, { 'status is 200': (r) => r.status === 200 });
 }
